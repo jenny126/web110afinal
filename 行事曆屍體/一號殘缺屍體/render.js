7c24dd2534cd4959032845f1922556bef666a0cc
jenny126 <s110910516@student.nqu.edu.tw>
2022-01-14 08:11:37 +0800

    我在彩虹嘔吐物中生出來的一堆屍體之一號沒傳到

---
+++ 行事曆屍體/一號殘缺屍體/render.js
@@ -0,0 +1,18 @@
+var catch_date = new Date();
+var catch_year = date.getFullYear();
+var catch_month = date.getMonth();
+var catch_day = catch__day.getDate();
+date = new Date(year, month, 1);
+
+const month =  ["January","Febrary","March","April","May","June","July","Auguest","September","October","November","December"];
+const month_olympic = [31,29,31,30,31,30,31,31,30,31,30,31];
+const month_normal = [31,28,31,30,31,30,31,31,30,31,30,31];
+const week=['Sun.','Mon.','Tue.','Wed.','Thurs.','Fri.','Sat.']
+
+function daysMonth(month, year) {
+    if ( year%4===0 && year%100!==0 || year%400===0 ) {
+        return (month_olympic[month]);
+    } else {
+        return (month_normal[month]);
+    }
+}
